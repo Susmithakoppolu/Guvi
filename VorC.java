acd30f5c3b67b3e1152885a42afa403b895611fa
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-07-25 23:16:51 -1200

    Update VorC.java

--- VorC.java
+++ VorC.java
@@ -11,7 +11,7 @@ class VorC
 	{
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
-		char ch=sc.nextChar();
+		char ch=sc.next().charAt(0);
 		if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
 		System.out.println("Vowel");
 		else if((ch>'a' && ch<='z') || (ch>'A' && ch<='Z'))
