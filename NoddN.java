b122bb38b4f25462225ddd5ed22e5d34dae6b7eb
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-07-26 19:52:27 -1200

    Update NoddN.java

--- NoddN.java
+++ NoddN.java
@@ -13,7 +13,7 @@ class NoddN
 		Scanner sc=new Scanner(System.in);
 		int n1=sc.nextInt();
 		int n2=sc.nextInt();
-	  for(int i=n1;i<=n2;i++)
+	  for(int i=n1+1;i<=n2;i++)
 		{
 		 if(i%2!=0)
 		 System.out.print(i);
