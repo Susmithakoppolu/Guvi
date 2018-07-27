5b0b953e99c622d9dc073abea69aede652871e49
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-07-26 22:52:27 -1200

    Update NevenN.java

--- NevenN.java
+++ NevenN.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+/* Name of the class has to be "Main" only if the class is public. */
 class NevenN
 {
 	public static void main (String[] args) throws java.lang.Exception
@@ -15,7 +16,7 @@ class NevenN
 		for(int i=n1+1;i<n2;i++)
 		{
 			if(i%2==0)
-			System.out.print(i+" ");
+			System.out.println(i);
 		}
 	}
 }
