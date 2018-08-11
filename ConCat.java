7f294e4ed20079f74e86e62729dba36af97deb6e
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-08-10 19:47:16 -1200

    Create ConCat.java

---
+++ ConCat.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class ConCat
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		String str1=sc.next();
+		System.out.println(str+str1);
+	}
+}
