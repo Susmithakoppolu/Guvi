c4aff67afc5c326e189b7a688347b3c9f60aeb95
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-08-12 17:13:29 -1200

    Create DigOrder.java

---
+++ DigOrder.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class DigOrder
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		LinkedList<Integer>stack=new LinkedList<Integer>();
+		while(n>0)
+		{
+			stack.push(n%10);
+			n=n/10;
+		}
+		while(!stack.isEmpty())
+		System.out.printf(stack.pop()+" ");
+		
+	}
+}
