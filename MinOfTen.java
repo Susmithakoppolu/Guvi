92135913ae39a3c85429794527a9e1685d58db9e
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-08-13 03:02:09 -1200

    Create MinOfTen.java

---
+++ MinOfTen.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class MinOfTen
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a[]=new int[10];
+		for(int i=0;i<10;i++)
+		a[i]=sc.nextInt();
+		int min=a[0];
+		for(int i=0;i<10;i++)
+		{
+		  if(min>a[i])
+		  min=a[i];
+		  else
+		  min=min;
+		}
+		System.out.println(min);
+	}
+}
