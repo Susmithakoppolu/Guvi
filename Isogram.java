5a88711db42adede1e37608e001530d14ec95ff5
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-08-14 19:39:02 -1200

    Create Isogram.java

---
+++ Isogram.java
@@ -0,0 +1,27 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Isogram
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		isIsogram(str);
+	}
+	static void isIsogram(String s){
+        String[] ary = s.split("");
+        Set<String> mySet = new HashSet<String>(Arrays.asList(ary));
+
+        if(s.length() == mySet.size()){
+            System.out.println("Yes");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
