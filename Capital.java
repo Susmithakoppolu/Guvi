f769eb01293751abb77bd120374708b353b57ef7
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-08-16 19:23:15 -1200

    Update Capital.java

--- Capital.java
+++ Capital.java
@@ -1,10 +1,6 @@
-/* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
 class Capital
 {
 	public static void main (String[] args) throws java.lang.Exception
@@ -12,14 +8,14 @@ class Capital
 		// your code goes here
 		Scanner sc=new Scanner(System.in);
 		String s=sc.nextLine();
-		String str=toFirstCharUpperAll(s);
-		System.out.println(str);
+	        toFirstCharUpperAll(s);
+		
 	}
-	public  static String toFirstCharUpperAll(String string){
+	public  static void toFirstCharUpperAll(String string){
                       StringBuffer sb=new StringBuffer(string);
                       for(int i=0;i<sb.length();i++)
-                       if(i==0 || sb.charAt(i-1)==' ')//first letter to uppercase by default
+                       if(i==0 || sb.charAt(i-1)==' ')
                        sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
-                    return sb.toString();
-}
+                       System.out.println(sb.toString());
+           }
 }
