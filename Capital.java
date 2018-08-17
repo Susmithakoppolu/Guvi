31b184483fbf6ff45e2991af6436a523fb12dfc2
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-08-16 19:03:44 -1200

    Create Capital.java

---
+++ Capital.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Capital
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String s1=s.substring(0,1).toUpperCase();
+		System.out.println(s1+s.substring(1));
+	}
+}
