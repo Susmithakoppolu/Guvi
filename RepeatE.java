541bb22e0c09a353e69a10f2590e233a1cc8a553
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-08-22 17:21:33 -1200

    Create RepeatE.java

---
+++ RepeatE.java
@@ -0,0 +1,36 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class RepeatE
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		findDuplicateChars(str);
+	}
+	public static void findDuplicateChars(String str){
+         
+        Map<Character, Integer> dupMap = new HashMap<Character, Integer>(); 
+        char[] chrs = str.toCharArray();
+        for(Character ch:chrs){
+            if(dupMap.containsKey(ch)){
+                dupMap.put(ch, dupMap.get(ch)+1);
+            } else {
+                dupMap.put(ch, 1);
+            }
+        }
+        Set<Character> keys = dupMap.keySet();
+         int maxValue = Collections.max(dupMap.values());
+        for(Character ch:keys){
+            if(dupMap.get(ch) == maxValue){
+                System.out.print(ch);
+            }
+        }
+    }
+}
