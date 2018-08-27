f0a381fa76587af753036479c09cc581ac29bc30
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-08-26 22:11:39 -1200

    Create LCM.java

---
+++ LCM.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class LCM
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	
+		Scanner sc=new Scanner(System.in);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int lcm=(n1<n2)?n1:n2;
+		while(true)
+		{
+			if(lcm%n1==0 && lcm%n2==0)
+			{
+			System.out.println(lcm);
+			break;
+			}
+			++lcm;
+		}
+	}
+}
