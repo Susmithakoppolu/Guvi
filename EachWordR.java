c61333fc94ebf5aedccff25753b03539e0da917b
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-03 18:25:46 -1200

    Create EachWordR.java

---
+++ EachWordR.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class EachWordR
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String words[]=str.split(" ");
+		String rev="";
+		for(String w:words){
+		StringBuilder sb=new StringBuilder(w);
+		sb.reverse();
+		rev+=sb+" ";
+		}
+		System.out.println(rev.trim());
+	}
+}
