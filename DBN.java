f2735dacb507f623e97e9790de1c4b14f09a734c
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-06 19:03:18 -1200

    Create DBN.java

---
+++ DBN.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class DBN
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		boolean flag=false;
+		for(int i=2;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				flag=true;
+			}
+		}
+		if(flag)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
