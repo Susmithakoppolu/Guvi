f1ee79ca1755dc4b4917f3a43c0ae47b9215d3e5
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-06 18:16:03 -1200

    Create OF.java

---
+++ OF.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class OF
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String rev="";
+		for(int i=1;i<=n;i++)
+		{
+			if(n%i==0)
+			{
+				if(i%2!=0)
+				
+			   	System.out.print(i+" ");
+			}
+		}
+		
+	}
+}
