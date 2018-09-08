e79f4797ad59ebea1c3dd470ecb1fc95f0f9c64b
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-07 16:09:04 -1200

    Create KSE.java

---
+++ KSE.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class KSE
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int n=sc.nextInt(),k=sc.nextInt();
+	 int temp;
+	 int a[]=new int[n];
+	 for(int i=0;i<n;i++)
+	 a[i]=sc.nextInt();
+	 System.out.println(a[k-1]);
+	
+}
+	
+}
