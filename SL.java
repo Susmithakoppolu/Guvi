8c32ebfad9ac08854bb6229ebb37d962fbb95fce
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-07 16:17:08 -1200

    Create SL.java

---
+++ SL.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class SL
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 String str=sc.next();
+	System.out.println(str.length());
+}
+	
+}
