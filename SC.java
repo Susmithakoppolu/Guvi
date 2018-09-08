81d91eadefa0b1bec0f8be4ec9a81c0aa0071392
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-07 16:24:49 -1200

    Create SC.java

---
+++ SC.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class SC
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 String str=sc.next();
+	 String str1=sc.next();
+             if(str.equals(str1))
+	System.out.println("yes");
+	else
+	System.out.println("no");
+}
+	
+}
