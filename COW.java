ce888b5062f6e800db2e27dd1ecf25e5cfe70801
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-09 03:11:29 -1200

    Create COW.java

---
+++ COW.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class COW
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String str1=sc.nextLine();
+		String words[]=str.split(" ");
+		int count=0;
+		for(int i=0;i<str1.length();i++)
+		{
+		
+				if(words[i].equals(str1))
+				count++;
+			
+		}
+		System.out.println(count);
+	}
+}
