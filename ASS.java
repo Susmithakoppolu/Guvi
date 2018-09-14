d70ca67b09ead6fb42b4908abcfc715566d76bb0
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-13 22:38:58 -1200

    Update and rename AS.java to ASS.java

---
+++ ASS.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class ASS
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		str=str+" "+"Answer";
+		System.out.println(str);
+	}
+}
