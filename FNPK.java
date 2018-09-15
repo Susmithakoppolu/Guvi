ade97a94f4d597c0c176f4c4fec43e5d1d35299a
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-14 17:29:58 -1200

    Create FNPK.java

---
+++ FNPK.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class FNPK
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  Scanner sc=new Scanner(System.in);
+	  int n=sc.nextInt(),k=sc.nextInt();
+	  System.out.println((int)Math.pow(n,k));
+	}
+}
