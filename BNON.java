c4f5bfcc5cb4ed45c12a860e06a62ccc959b4e11
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-17 02:13:20 -1200

    Create BNON.java

---
+++ BNON.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class BNON { 
+      
+    
+    public static void main(String args[]) 
+    {
+    	Scanner sc=new Scanner(System.in);
+    	int n=sc.nextInt();
+    	System.out.println(~n);
+    } 
+} 
+  
