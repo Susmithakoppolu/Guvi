31c9d5e9af750acf22137c22edea48150e0acf4f
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-09-17 02:25:59 -1200

    Create LSON.java

---
+++ LSON.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class LSON
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt(),k=sc.nextInt();
+		int r=n<<k;
+		System.out.println(r);
+	}
+}
