cd5786d18d9f38c1ca73c4e32119535fbe0df9ca
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-11-29 03:01:14 -1200

    Create HTD.java

---
+++ HTD.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class HTD
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int decimal=Integer.parseInt(str,16);
+        System.out.println(decimal);
+	}
+}
