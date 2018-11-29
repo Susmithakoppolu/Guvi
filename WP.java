cc97a1330fe8e2c901ffe4793617d816203e530c
Susmithakoppolu <41731270+Susmithakoppolu@users.noreply.github.com>
2018-11-28 19:20:28 -1200

    Create WP.java

---
+++ WP.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class WP
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.nextLine();
+		String s2=sc.next();
+		String[] s3=s1.split(" ");
+		for(int i=0;i<s3.length;i++)
+		{
+			if(s3[i].charAt(0)==s2.charAt(0))
+			{
+				break;	
+			}
+		}
+		System.out.println(i+1);
+	}
+}
